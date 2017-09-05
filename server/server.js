934755c66493ceed919dad2f890180f38f1c77d2
aarondv1 <aarondv1@gmail.com>
2017-09-04 23:36:37 -0400

    Simple Create/Post Endpoint created

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 const {mongoose} = require('./db/mongoose');
 const {CreditCard} = require('./models/CreditCard');
 
-
-
+var bodyparser = require('body-parser');
+var express = require('express');
+var app = express();
 // SERVER IS FOR THE ENDPOINTS
+
+app.use(bodyparser.json());
+app.post('/order', (req,res)=>{
+   
+    var order = new CreditCard(req.body);
+
+    order.save()
+    .then(doc=>{res.status(201).send(doc)}, e=>{res.status(400).send(e)})
+    
+
+
+    
+
+});
+
+
+var PortNumber = 3000;
+app.listen(PortNumber,()=>{console.log(`Listening on Port ${PortNumber}`)});
