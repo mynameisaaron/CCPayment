372b6a0bcf5668eda7ed5b38e2f3ecb9a7d14e9d
aarondv1 <aarondv1@gmail.com>
2017-09-09 21:26:56 -0400

    Setup for Mlab-Database on Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/CreditCard');
+
+var database = process.env.MONGODB_URI || 'mongodb://localhost:27017/CreditCard';
+
+mongoose.connect(database);
 
 module.exports = {mongoose};
 //for export to the server.js only
